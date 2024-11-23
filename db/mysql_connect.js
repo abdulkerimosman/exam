048501dcb3df2e079e2e359b7e684552fc9bcbd0
Abdulkerim <abdulkerimissa1@gmail.com>
2024-11-23 10:27:44 +0300

    new change

---
+++ db/mysql_connect.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+
+let dbConn = mysql.createConnection({
+  user: root,
+  password: null,
+  database: chatgpt2,
+  host: localhost  
+})
+
+dbConn.connect((err)=>{
+  if(!err) {
+    console.log('veritabana baglanildi');
+  } else {
+    console.log('veritabanina baglanilmadi')
+  }
+})
+
+module.exports = dbConn;
